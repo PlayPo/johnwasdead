9c4936d5846a12f2f5d9d2d689268de817cc65e9
PlayPo <juegapo2022@gmail.com>
2025-11-04 00:27:56 -0300

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,1 +1,19 @@
+var i; 
+
+var arLinks = new Array();
+
+arLinks[0] = "https://johnwasdead.com/";
+arLinks[1] = "https://johnwasdead.com/neverending_dream";
+arLinks[2] = "https://johnwasdead.com/in_m_yhead";
+arLinks[3] = "https://johnwasdead.com/getout";
+arLinks[4] = "https://johnwasdead.com/lost_in_my_mind";
+arLinks[5] = "https://johnwasdead.com/misleading";
+arLinks[6] = "https://johnwasdead.com/underground";
+
+i = Math.floor( Math.random() * arLinks.length );
+window.location = arLinks[i];
+
+
+
+
 
