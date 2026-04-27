8d31831bf8138b4111e697979e300e23861f2f2a
PlayPo <juegapo2022@gmail.com>
2026-04-27 12:19:04 -0300

    Update scroll.js

--- scroll.js
+++ scroll.js
@@ -1,8 +1,8 @@
 const autoScroll = setInterval(() => {
     window.scrollBy(0, 10);
     
-    // Detener si llegamos al final de la página
+
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
         clearInterval(autoScroll);
     }
-}, 0);
+}, 1);
